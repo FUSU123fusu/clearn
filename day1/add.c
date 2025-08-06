68811e89d0c0d87f47a50470b5cff931079a08fa
FUSU123fusu <88240205+FUSU123fusu@users.noreply.github.com>
2025-08-06 22:38:50 +0800

    Add files via upload

---
+++ day1/add.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+
+//全局变量定义
+
+// int g_val = 2022;
+
+// int add(int x, int y) {
+//     int z = 0;
+//     z = x + y;
+//     return z;
+// }
+//如果加上static关键字，则只能在当前文件中使用

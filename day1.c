b45d2446aecaccc733f8f505eefd0ea2fa88949a
FUSU123fusu <88240205+FUSU123fusu@users.noreply.github.com>
2025-08-06 20:48:36 +0800

    Add files via upload

---
+++ day1.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+typedef unsigned int uint;
+typedef struct Node {
+    int data;
+    struct Node *next;
+} Node;
+
+// int add (int x,int y)
+// {
+// int z = 0;
+// z =x+y;
+// return z;
+// }
+// #include <stdio.h>
+// int main (void){
+//     int n1 = 0;
+//     int n2 = 0;
+//     //输入
+//     scanf("%d,%d",&n1,&n2);
+//     //int sum = n1+n2;
+//     int sum = add(n1,n2);
+//     printf("%d\n",sum);
+//     return 0;
+// }
+int main (void) {
+    // int a[20] ={10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29};
+    // // printf("%d\n", a[8]);
+    // int i = 0;
+    // for (i = 0; i < 20; i++) {
+    //     printf("%d\n", a[i]);
+    // }
+    //sizeof是个单目操作符
+    // int a = 0;
+    // int b = 0;
+    // printf("请输入两个整数：");
+    // scanf("%d,%d",&a,&b);
+    // int r = a > b ? a :b;
+    // printf("较大值为：%d\n",r);
+    uint a = 1;
+    Node n2;    
+    return 0;
+}
